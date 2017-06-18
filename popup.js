27787f3ec74d821339c1776280976a0723187c6c
chentairan <chentairan@weidian.com>
2017-06-19 00:10:41 +0800

    some data storage function and security policy

--- popup.js
+++ popup.js
@@ -17,7 +17,6 @@ var status = null;
 function updateUiState() {
   // state: login logged registration reg-success reg-failed login-failed search monitor specified-search-success list-search-success search-failed connect
   if (port) {
-    console.log('port')
     if(status == 'login'){
       document.getElementById('connect-div').style.display = 'none';
       document.getElementById('login-div').style.display = 'block';
@@ -144,37 +143,49 @@ function updateUiState() {
     }
   }
   else {
-    if (status == 'connect'){
-      document.getElementById('connect-div').style.display = 'block';
-      document.getElementById('login-div').style.display = 'none';
-      document.getElementById('reg-div').style.display = 'none';
-      document.getElementById('logged-div').style.display = 'none';
-      document.getElementById('search-div').style.display = 'none';
-      document.getElementById('result-div').style.display = 'none';
-      document.getElementById('monitor-div').style.display = 'none';
-      document.getElementById('reason-div').style.display = 'none';
-      document.getElementById('back-div').style.display = 'none';
-    }
+    console.log('not port');
+    document.getElementById('connect-div').style.display = 'block';
+    document.getElementById('login-div').style.display = 'none';
+    document.getElementById('reg-div').style.display = 'none';
+    document.getElementById('logged-div').style.display = 'none';
+    document.getElementById('search-div').style.display = 'none';
+    document.getElementById('result-div').style.display = 'none';
+    document.getElementById('monitor-div').style.display = 'none';
+    document.getElementById('reason-div').style.display = 'block';
+    document.getElementById('back-div').style.display = 'none';
     // do something
   }
 }
 
 // button function
-function connect(){
-  var execResult = 'success';
-  if (execResult == 'success'){
-    status = 'login';
-    updateUiState()
-  }
+
+function connect() {
+  var hostName = "com.blaketairan.mypasswd.host";
+  appendMessage("Connecting to native messaging host <b>" + hostName + "</b>")
+  console.log('finish appendMessage');
+  port = chrome.runtime.connectNative(hostName);
+  port.onMessage.addListener(onNativeMessage);
+  port.onDisconnect.addListener(onDisconnected);
+  status="login";
+  updateUiState();
 }
 
 function login(){
+  var temp_account = document.getElementById('username').value;
+  var temp_passwd = document.getElementById('passwd').value;
+  var temp_remember = document.getElementById('saveAccount').checked;
+  chrome.storage.local.set({"account":temp_account, "password":temp_passwd, "save":temp_remember});
   var execResult = 'success';
   if (execResult == 'success'){
     status = 'logged';
     updateUiState()
   }
-
+  chrome.storage.local.get("account",function(data){
+    console.log(data.account);
+  });
+  chrome.storage.local.get("password", function(data){
+    console.log(data.password);
+  });
 }
 
 function register(){
@@ -196,6 +207,7 @@ function search(){
 }
 
 function toLogin(){
+  chrome.storage.local.clear();
   var execResult = 'success';
   if (execResult == 'success'){
     status = 'login';
@@ -238,6 +250,7 @@ function toMonitor(){
 
 
 function appendMessage(text) {
+  console.log("appendMessage");
   document.getElementById('reason-div').innerHTML += "<p>" + text + "</p>";
 }
 
@@ -259,20 +272,23 @@ function onDisconnected() {
   updateUiState();
 }
 
-function connect() {
-  var hostName = "com.blaketairan.mypasswd.host";
-  appendMessage("Connecting to native messaging host <b>" + hostName + "</b>")
-  port = chrome.runtime.connectNative(hostName);
-  port.onMessage.addListener(onNativeMessage);
-  port.onDisconnect.addListener(onDisconnected);
-  status="login";
-  updateUiState();
+// clean data
+function cleanData() {
+  chrome.storage.local.get('save', function(data){
+    if (data.save == true){
+      console.log('not clear');
+      return
+    }
+    chrome.storage.local.clear();
+    console.log('clear');
+  });
 }
 
 // trigger
 document.addEventListener('DOMContentLoaded', function () {
   document.getElementById('connect-button').addEventListener(
       'click', connect);
+  window.onunload = cleanData();
   // document.getElementById('send-message-button').addEventListener(
   //     'click', sendNativeMessage);
   document.getElementById('login-btn').addEventListener('click',login);
@@ -280,14 +296,13 @@ document.addEventListener('DOMContentLoaded', function () {
   document.getElementById('reg-btn').addEventListener('click',register);
   document.getElementById('search-mode-btn').addEventListener('click',toSearch);
   document.getElementById('monitor-mode-btn').addEventListener('click',toMonitor);
+  document.getElementById('log-out-btn').addEventListener('click',toLogin);
   document.getElementById('search-btn').addEventListener('click',search);
   document.getElementById('backToLogin-btn').addEventListener('click',toLogin);
   document.getElementById('backToSearch-btn').addEventListener('click',toSearch);
   var inputclass = document.getElementsByClassName("ipb");
   for(var i = 0;i < inputclass.length; i++){
     inputclass[i].addEventListener('click', function(){
-    console.log(this)
-    console.log(this.value)
     this.value='';
   })
   };
